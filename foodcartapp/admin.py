e2f3be0dd83a3b59080a00f6773765cf08ecea3d
Amirzhan Albariyev <albariev123@gmail.com>
2025-10-09 03:35:59 +0800

    Redirect for manager

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from django.shortcuts import reverse
+from django.shortcuts import reverse, redirect
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.forms import ModelForm
+from django.http import HttpResponseRedirect
 
 from .models import Product
 from .models import ProductCategory
@@ -114,5 +117,14 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['firstname', 'lastname', 'phonenumber', 'address',]
+    list_display = ['firstname', 'lastname', 'phonenumber', 'address']
     inlines = [OrderItemInline]
+
+    def response_post_save_change(self, request, obj):
+        res = super().response_post_save_change(request, obj)
+        next_url = request.GET.get('next')
+
+        if next_url and url_has_allowed_host_and_scheme(next_url, allowed_hosts={request.get_host()}):
+            self.message_user(request, "Заказ сохранён, возвращаем вас назад.")
+            return HttpResponseRedirect(next_url)
+        return res
